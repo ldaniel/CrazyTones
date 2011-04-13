227dae97f32f15562479e0737d631449dcb5af9a
ldaniel <contato@leandrodaniel.com>
2011-04-13 18:52:11 -0300

    Refactoring das classes que tratam Wave

--- src/CrazyTones.Solution/CrazyTones.Core/WaveGenerator.cs
+++ src/CrazyTones.Solution/CrazyTones.Core/WaveGenerator.cs
@@ -3,46 +3,15 @@ using System.IO;
 
 namespace CrazyTones.Core
 {
-    public enum WaveExampleType
-    {
-        ExampleSineWave = 0
-    }
-
     public class WaveGenerator
     {
-        private readonly WaveHeader _header;
-        private readonly WaveFormatChunk _format;
-        private readonly WaveDataChunk _data;
+        private IWaveType _wave;
         private FileStream _fileStream;
         private BinaryWriter _writer;
 
-        public WaveGenerator(WaveExampleType type)
-        {          
-            _header = new WaveHeader();
-            _format = new WaveFormatChunk();
-            _data = new WaveDataChunk();            
-
-            switch (type)
-            {
-                case WaveExampleType.ExampleSineWave:
-                    uint numSamples = _format.SamplesPerSec * _format.NumberOfChannels;
-                    _data.ShortArray = new short[numSamples];
-                    int amplitude = 32760;  
-                    double freq = 440.0f;   
-                    double t = (Math.PI * 2 * freq) / (_format.SamplesPerSec * _format.NumberOfChannels);
-
-                    for (uint i = 0; i < numSamples - 1; i++)
-                    {
-                        for (int channel = 0; channel < _format.NumberOfChannels; channel++)
-                        {
-                            _data.ShortArray[i + channel] = Convert.ToInt16(amplitude * Math.Sin(t * i));
-                        }                        
-                    }
-
-                    _data.LenghtOfHeaderInBytes = (uint)(_data.ShortArray.Length * (_format.BitsPerSample / 8));
-
-                    break;
-            }          
+        public WaveGenerator()
+        {
+            _wave = new WaveSine();   
         }
 
         public void Save(string filePath)
@@ -68,9 +37,10 @@ namespace CrazyTones.Core
 
         private void WriteData()
         {
-            _writer.Write(_data.ChunkID.ToCharArray());
-            _writer.Write(_data.LenghtOfHeaderInBytes);
-            foreach (var dataPoint in _data.ShortArray)
+            _writer.Write(_wave.GetDataChunkID().ToCharArray());
+            _writer.Write(_wave.GetDataLenghtOfHeaderInBytes());
+            
+            foreach (var dataPoint in _wave.GetDataShortArray())
             {
                 _writer.Write(dataPoint);
             }
@@ -82,21 +52,21 @@ namespace CrazyTones.Core
 
         private void WriteFormat()
         {
-            _writer.Write(_format.ChunkID.ToCharArray());
-            _writer.Write(_format.LenghtOfHeaderInBytes);
-            _writer.Write(_format.FormatTag);
-            _writer.Write(_format.NumberOfChannels);
-            _writer.Write(_format.SamplesPerSec);
-            _writer.Write(_format.AvgBytesPerSec);
-            _writer.Write(_format.SampleFrameSizeInBytes);
-            _writer.Write(_format.BitsPerSample);
+            _writer.Write(_wave.GetFormatChunkID().ToCharArray());
+            _writer.Write(_wave.GetFormatLenghtOfHeaderInBytes());
+            _writer.Write(_wave.GetFormatTag());
+            _writer.Write(_wave.GetFormatNumberOfChannels());
+            _writer.Write(_wave.GetFormatSamplesPerSec());
+            _writer.Write(_wave.GetFormatAvgBytesPerSec());
+            _writer.Write(_wave.GetFormatSampleFrameSizeInBytes());
+            _writer.Write(_wave.GetFormatBitsPerSample());
         }
 
         private void WriteHeader()
         {
-            _writer.Write(_header.GroupID.ToCharArray());
-            _writer.Write(_header.FileLength);
-            _writer.Write(_header.RiffType.ToCharArray());
+            _writer.Write(_wave.GetHeaderGroupID().ToCharArray());
+            _writer.Write(_wave.GetHeaderFileLength());
+            _writer.Write(_wave.GetHeaderRiffType().ToCharArray());
         }
     }
 }
