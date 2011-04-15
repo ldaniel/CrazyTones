24c6edbb30c0e6d88f86326473ad05dee8946dcb
ldaniel <contato@leandrodaniel.com>
2011-04-14 21:53:39 -0300

    Refactoring.

---
+++ src/CrazyTones.Solution/CrazyTones.Core/Chunk/WaveDataChunk.cs
@@ -0,0 +1,16 @@
+﻿namespace CrazyTones.Core
+{
+    public class WaveDataChunk
+    {
+        public string ChunkID;    
+        public uint LenghtOfHeaderInBytes;   
+        public short[] ShortArray;  
+
+        public WaveDataChunk()
+        {
+            ShortArray = new short[0];
+            LenghtOfHeaderInBytes = 0;
+            ChunkID = "data";
+        }   
+    }
+}
---
+++ src/CrazyTones.Solution/CrazyTones.Core/Chunk/WaveHeader.cs
@@ -0,0 +1,16 @@
+﻿namespace CrazyTones.Core
+{
+    public class WaveHeader
+    {
+        public string GroupID; 
+        public uint FileLength;
+        public string RiffType; 
+
+        public WaveHeader()
+        {
+            FileLength = 0;
+            GroupID = "RIFF";
+            RiffType = "WAVE";
+        }
+    }
+}
